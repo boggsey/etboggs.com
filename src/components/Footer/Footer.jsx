001c2152933501b6d9e01b5378530be92d32d241
boggsey <eric@mlemedia.com>
2019-05-05 21:49:32 -0400

    Refactoring Footer for emotion and tailwind

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,41 @@
+/* global tw */
 import React, { Component } from "react";
+import styled from "react-emotion";
 import Nav from "./../Nav/Nav";
-import "./Footer.scss";
+
+const FooterWrapper = styled.div`
+  ${tw("flex flex-col items-center justify-center")};
+  min-height: 20vh;
+  background: #111;
+  color: #cecece;
+
+  nav {
+    ${tw("text-center")};
+
+    a {
+      ${tw("no-underline inline-block text-white uppercase")};
+      padding: 0 10px;
+      font-family: "Roboto";
+      text-transform: uppercase;
+      font-weight: 600;
+      padding-right: 5px;
+    }
+  }
+
+  .made-with {
+    text-align: center;
+    color: #555;
+    font-size: 10px;
+    padding: 5px 0;
+  }
+`;
 
 class Footer extends Component {
   render() {
     return (
-      <footer className="footer">
+      <FooterWrapper>
         <Nav />
-      </footer>
+      </FooterWrapper>
     );
   }
 }
