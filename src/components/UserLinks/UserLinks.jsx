b5c923c90c04efeee4c44d8e75de8866fc6c9748
boggsey <eric@mlemedia.com>
2019-05-06 00:18:01 -0400

    Refactoring UserLinks for emotion and tailwind. also, removing FeaturedPosts

--- src/components/UserLinks/UserLinks.jsx
+++ src/components/UserLinks/UserLinks.jsx
@@ -1,21 +1,27 @@
+/* global tw */
 import React, { Component } from "react";
+import styled from "react-emotion";
 import { FaLinkedin, FaTwitter, FaGithub } from "react-icons/fa";
-import "./UserLinks.scss";
+
+const UserLinksWrapper = styled.div`
+  ${tw("w-full items-center justify-center md:justify-start flex flex-wrap")};
+`;
 
 class UserLinks extends Component {
   render() {
+    const { size } = this.props;
     return (
-      <div>
+      <UserLinksWrapper>
         <a href="https://twitter.com/etboggs">
-          <FaTwitter size={this.props.size} />
+          <FaTwitter size={size} />
         </a>
         <a href="https://www.linkedin.com/in/etboggs/">
-          <FaLinkedin size={this.props.size} />
+          <FaLinkedin size={size} />
         </a>
         <a href="https://github.com/boggsey">
-          <FaGithub size={this.props.size} />
+          <FaGithub size={size} />
         </a>
-      </div>
+      </UserLinksWrapper>
     );
   }
 }
