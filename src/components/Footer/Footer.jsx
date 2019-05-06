74ded246715a6903fa01180dd43dd19791d35f2c
boggsey <eric@mlemedia.com>
2019-05-05 21:54:17 -0400

    Updating footer styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,26 +7,16 @@ const FooterWrapper = styled.div`
   ${tw("flex flex-col items-center justify-center")};
   min-height: 20vh;
   background: #111;
-  color: #cecece;
 
   nav {
     ${tw("text-center")};
 
     a {
-      ${tw("no-underline inline-block text-white uppercase")};
-      padding: 0 10px;
+      ${tw(
+        "no-underline inline-block text-white uppercase pl-4 pr-4 font-semibold"
+      )};
       font-family: "Roboto";
-      text-transform: uppercase;
-      font-weight: 600;
-      padding-right: 5px;
     }
-  }
-
-  .made-with {
-    text-align: center;
-    color: #555;
-    font-size: 10px;
-    padding: 5px 0;
   }
 `;
 
